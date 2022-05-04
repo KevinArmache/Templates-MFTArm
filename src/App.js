1c3ee8c4bb3c866cddb0f87ba1d3125c160865e4
KevinArmache <kevinarmache@gmail.com>
2022-05-04 16:20:35 +0100

    add: end with rotate div of next section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 
 import Main from "./components/main/Main";
 import Home from "./components/Home/Home";
+import Features from "./components/features/features";
 
 function App() {
   return (
     <div className="App">
       <Main />
       <Home />
+      <Features />
     </div>
   );
 }
