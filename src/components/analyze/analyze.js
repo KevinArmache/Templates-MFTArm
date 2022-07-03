2ab60602268d571e84ab0fa75e4ac65d84a75e70
KevinArmache <kevinarmache@gmail.com>
2022-07-03 05:49:57 +0100

    add: last element of analyze section

--- src/components/analyze/analyze.js
+++ src/components/analyze/analyze.js
@@ -113,6 +113,39 @@ function Analyze() {
               </div>
             </div>
           </div>
+
+          {/* ELEMENT FOUR */}
+
+          <div className="four-bar-analyze">
+            <div className="container-four-bar">
+              <div className="circle-analyze">
+                <div className="circle-gray">
+                  <div className="circle-orange">
+                    <div className="circle-inside">
+                      {" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="25"
+                        height="25"
+                        fill="#F9D04C"
+                        class="bi bi-arrow-right"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="circle-text">
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
